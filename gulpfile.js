4b51d97c0605f3a1e3976d5232fe26497343506a
yasminsousa19 <yasminfsousa@outlook.com>
2025-03-27 18:47:44 -0300

    criando o hero e importando a fonte Avenir

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 const gulp = require('gulp'); //importa o gulp
 const sass = require('gulp-sass')(require('sass')) //importa o sass
+const imagemin = require('gulp-imagemin')
 
 function styles(){
     return gulp.src('./src/styles/*.scss') //essa função busca todos os arquivos scss dentro da pasta styles
@@ -7,7 +8,14 @@ function styles(){
         .pipe(gulp.dest('./dist/css')) //aqui ele vai salvar os arquivos
 }
 
-exports.default = styles;
+function images(){
+    return gulp.src('./src/images/**/*') //essa função busca todos os arquivos dentro da pasta images
+        .pipe(imagemin()) //aqui mostra que os estilos que sairem em scss vai ser comprimido
+        .pipe(gulp.dest('./dist/images')) 
+}
+
+exports.default = gulp.parallel(styles, images); //esse parallel vai executar ambos os plugins juntamente
+
 exports.watch = function(){
     gulp.watch('./src/styles/*.scss', gulp.parallel(styles)); //aqui ele vai monitorar os arquivos que serão alterados
 }
